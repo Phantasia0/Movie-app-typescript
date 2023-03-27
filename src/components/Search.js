6e1b121ba7aa047e612ab769bbc5dcd4376900ef
Phantasia0 <ncprog1@gmail.com>
2023-03-28 06:11:07 +0900

    영화 검색 기능 및 디자인 추가

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import { Component } from "../core/template";
+import movieStore, { searchMovies } from "../store/movie";
 
 export default class Search extends Component {
   render() {
@@ -10,17 +11,19 @@ export default class Search extends Component {
 
     const inputEl = this.el.querySelector("input");
     inputEl.addEventListener("input", () => {
-      //
+      movieStore.state.searchText = inputEl.value;
     });
     inputEl.addEventListener("keydown", (event) => {
-      if (event.key === "Enter" && inputEl.value.trim() !== "") {
-        //
+      if (event.key === "Enter" && movieStore.state.searchText.trim()) {
+        searchMovies(1);
       }
     });
 
     const btnEl = this.el.querySelector(".btn");
     btnEl.addEventListener("click", () => {
-      //
+      if (movieStore.state.searchText.trim()) {
+        searchMovies(1);
+      }
     });
   }
 }

fe8e8494bdc7cbf8ecf728fe25f6cbe070db19a5
Phantasia0 <ncprog1@gmail.com>
2023-04-04 02:30:56 +0900

    CommonJS 로 설정 변경

--- api/movie.ts
+++ api/movie.ts
@@ -1,19 +1,12 @@
-import fetch from "node-fetch";
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import fetch from "node-fetch";
 
 const { APIKEY } = process.env;
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
-  const { title, page, id } = JSON.parse(request.body);
-  //prettier-ignore
-  const url = id 
-    ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
-    : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`;
-
+  const { title, page, id } = JSON.parse(request.body as string);
+  const url = id ? `https://www.omdbapi.com/?apikey=${APIKEY}&i=${id}&plot=full` : `https://www.omdbapi.com/?apikey=${APIKEY}&s=${title}&page=${page}`;
   const res = await fetch(url);
   const json = await res.json();
-  //prettier-ignore
-  response
-    .status(200)
-    .json(json);
+  response.status(200).json(json);
 }
